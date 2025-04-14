9fb790de95b9f468590a9f42a01ace56ef6cedbc
Yeray Yas <yerayyas@gmail.com>
2025-04-14 14:07:39 +0100

    Add FirebaseModule documentation

--- app/src/main/java/com/yerayyas/chatappkotlinproject/di/FirebaseModule.kt
+++ app/src/main/java/com/yerayyas/chatappkotlinproject/di/FirebaseModule.kt
@@ -10,21 +10,44 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+/**
+ * Dagger Hilt module for providing Firebase-related dependencies.
+ *
+ * This module installs bindings into the SingletonComponent,
+ * meaning the provided instances will live as long as the application.
+ */
 @Module
 @InstallIn(SingletonComponent::class)
 object FirebaseModule {
 
+    /**
+     * Provides a singleton instance of [FirebaseAuth].
+     *
+     * @return An instance of FirebaseAuth.
+     */
     @Provides
     @Singleton
     fun provideFirebaseAuth(): FirebaseAuth {
         return FirebaseAuth.getInstance()
     }
 
+    /**
+     * Provides the root [DatabaseReference] of the Firebase Realtime Database.
+     *
+     * This reference can be used to read/write to the database.
+     *
+     * @return A DatabaseReference pointing to the root node.
+     */
     @Provides
     fun provideFirebaseDatabase(): DatabaseReference {
         return FirebaseDatabase.getInstance().reference
     }
 
+    /**
+     * Provides a singleton instance of [FirebaseStorage].
+     *
+     * @return An instance of FirebaseStorage for uploading and downloading files.
+     */
     @Provides
     @Singleton
     fun provideFirebaseStorage(): FirebaseStorage {

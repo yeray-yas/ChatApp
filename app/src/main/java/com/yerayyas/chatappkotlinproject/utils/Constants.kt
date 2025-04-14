394cce1cb935d729df0caaff703c08b0f4ddf8ac
Yeray Yas <yerayyas@gmail.com>
2025-04-14 20:18:51 +0100

    Add Constants class documentation

--- app/src/main/java/com/yerayyas/chatappkotlinproject/utils/Constants.kt
+++ app/src/main/java/com/yerayyas/chatappkotlinproject/utils/Constants.kt
@@ -1,9 +1,29 @@
 package com.yerayyas.chatappkotlinproject.utils
 
+/**
+ * This object contains constant values used throughout the application.
+ *
+ * Constants include error messages for loading and sending messages or images,
+ * as well as a constant for animation duration.
+ */
 object Constants {
+    /**
+     * Error message displayed when there is an issue loading messages.
+     */
     const val ERROR_LOADING_MESSAGES = "Error loading messages"
+
+    /**
+     * Error message displayed when there is an issue sending a message.
+     */
     const val ERROR_SENDING_MESSAGE = "Error sending message"
+
+    /**
+     * Error message displayed when there is an issue sending an image.
+     */
     const val ERROR_SENDING_IMAGE = "Error sending image"
 
+    /**
+     * The duration of animations in milliseconds.
+     */
     const val ANIMATION_DURATION = 300
 }
